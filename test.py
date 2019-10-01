a37e777974d49e62d525333cf47d0337326b6699
Kwon-YJ <kyj1435@gmail.com>
2019-10-02 05:26:57 +0900

    AES

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+'''
 # Python3 code to demonstrate working of 
 # Converting String to binary 
 # Using join() + ord() + format() 
@@ -15,3 +16,19 @@ res = ''.join(format(ord(i), 'b') for i in test_str)
 
 # printing result  
 print("The string after binary conversion : " + res)
+'''
+
+
+
+a = "00001000"
+b = "0101101"
+#y = int(a, 2)^int(b, 2)
+#print(bin(y)[2:].zfill(len(a)))
+
+#[output: 00010100000000001110000101010001001]
+
+
+
+print(bin(int(a, 2)^int(b, 2))[2:].zfill(len(a)))
+
+print(type(bin(int(a, 2)^int(b, 2))[2:].zfill(len(a))))
